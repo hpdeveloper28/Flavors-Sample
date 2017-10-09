731e308e06bc0707e31eac5d6a4035770d8eb767
hiren <hpatel@mobiquityinc.com>
2017-10-09 13:01:37 +0530

    Initial commit with 2 flavors for BASE_URL

---
+++ app/src/main/java/com/hpdeveloper/flavorssample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hpdeveloper.flavorssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.txtBaseUrl);
+        textView.setText(AppConfig.BASE_URL);
+    }
+}
---
+++ app/src/qa/java/com/hpdeveloper/flavorssample/AppConfig.java
@@ -0,0 +1,10 @@
+package com.hpdeveloper.flavorssample;
+
+/**
+ * Created by hirenpatel on 09/10/17.
+ */
+
+public class AppConfig {
+
+    public static final String BASE_URL = "www.xyz.com";
+}
